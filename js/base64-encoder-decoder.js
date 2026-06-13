9711e2a7897c6b850def4a2a91fca2d65f03f9e8
KingIrshadh007 <kingirshadh1998@gmail.com>
2026-06-13 12:41:29 +0530

    Update base64-encoder-decoder.js

--- js/base64-encoder-decoder.js
+++ js/base64-encoder-decoder.js
@@ -1,1 +1,49 @@
+function encodeBase64(){
 
+    const input =
+    document.getElementById("base64Input").value;
+
+    if(!input){
+
+        document.getElementById("base64Result").value =
+        "Please enter text";
+
+        return;
+    }
+
+    document.getElementById("base64Result").value =
+    btoa(input);
+}
+
+function decodeBase64(){
+
+    const input =
+    document.getElementById("base64Input").value;
+
+    try{
+
+        document.getElementById("base64Result").value =
+        atob(input);
+
+    }catch(error){
+
+        document.getElementById("base64Result").value =
+        "Invalid Base64 string";
+    }
+}
+
+function copyBase64Result(){
+
+    const result =
+    document.getElementById("base64Result");
+
+    result.select();
+
+    document.execCommand("copy");
+}
+
+function clearBase64(){
+
+    document.getElementById("base64Input").value = "";
+    document.getElementById("base64Result").value = "";
+}
